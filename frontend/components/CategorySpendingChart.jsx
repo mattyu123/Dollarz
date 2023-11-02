957e1c758e1fecaa45277a053df0d8cffaa26cc5
mattyu123 <matthewyu100@gmail.com>
2023-11-01 23:35:07 -0400

    render chart on the home page

--- frontend/components/CategorySpendingChart.jsx
+++ frontend/components/CategorySpendingChart.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-import { BarChart, Bar } from "recharts";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 export default function CategorySpendingChart({ data, selectMonth }) {  
+
   if (!data) {
-    return <div>Chart is Loading...</div>
+    return <p>Chart loading</p>
   }
 
-  const currentMonthExpenses = data.filter((expense) =>
-    expense.date.startsWith(selectMonth)
-  );
+  const currentMonthData = data
+    .filter(expense => expense.date.startsWith(selectMonth))
 
-  console.log(currentMonthExpenses)
+  console.log(currentMonthData)
 
   return (
-    <h1>Chart going to go here</h1>
-  )
+      <BarChart
+        width={500}
+        height={300}
+        data={currentMonthData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date"/>
+        <YAxis/>
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value"/>
+      </BarChart>
+  );
 }
