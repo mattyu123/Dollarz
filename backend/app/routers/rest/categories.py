7bb37b2200967c0d93c95d3a49c765f1b1fa1965
Prestin Liu <liuprestin@proton.me>
2023-11-04 19:42:41 -0700

    add try-except statement to catch errors better

--- backend/app/routers/rest/categories.py
+++ backend/app/routers/rest/categories.py
@@ -10,23 +10,30 @@ router = APIRouter()
 
 @router.get("/categories", response_model=List[CategoryCreate])
 async def read_categories():
-    categories = await fetch_all_categories()
-    return categories
+    try:
+        categories = await fetch_all_categories()
+        return categories
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=f"/categories - line 17: Internal Server Error: {ex}")
 
 @router.post("/categories", response_model=CategoryCreate)
 async def add_category(category: CategoryCreate):
-    category_data = category.dict()
-    new_category = await create_category(category_data)
-    created_category = await fetch_category(new_category.inserted_id)
-    return created_category
+    try:
+        category_data = category.dict()
+        new_category = await create_category(category_data)
+        created_category = await fetch_category(new_category.inserted_id)
+        return created_category
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=f"/categories - line 26: Internal Server Error: {ex}")
 
 @router.delete("/categories/{category_id}")
 async def delete_category_item(category_id: str):
     if not ObjectId.is_valid(category_id):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ObjectId format")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="/categories - line 32: Invalid ObjectId format")
     
     result = await delete_category({'_id': ObjectId(category_id)})
+    
     if result.deleted_count:
         return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "success", "message": "Category deleted"})
     
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Category with id {category_id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"/categores - line 39: id {category_id} not found")
