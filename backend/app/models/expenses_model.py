f4890bab827dc8a5c38967817e8ce60e615f0c63
Prestin Liu <liuprestin@proton.me>
2023-11-04 17:40:54 -0700

    beta: adding in delete expenses REST route

--- backend/app/models/expenses_model.py
+++ backend/app/models/expenses_model.py
@@ -5,7 +5,13 @@ expense_collection = database.get_collection("expenses")
 
 # Add an expense to the database
 async def create_expense(expense_data):
-    await expense_collection.insert_one(expense_data)
+    result = await expense_collection.insert_one(expense_data)
+    return result
+
+# delete a expense in the database
+async def delete_expense(expense_id):
+   result = await expense_collection.delete_one(expense_id)
+   return result
 
 #Fetch all of the expense items in the expenses collection and return it as an array
 async def fetch_all_expenses():
--- backend/app/routers/rest/expenses.py
+++ backend/app/routers/rest/expenses.py
@@ -15,3 +15,14 @@ async def add_expense(expense: ExpenseCreate):
 @router.get("/expenses")
 async def list_expenses():
     return await fetch_all_expenses()
+
+@router.delete("/expenses/{expense_id}")
+async def delete_expense_item(expense_id: str):
+    if not ObjectId.is_valid(expense_id):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ObjectId format")
+    
+    result = await delete_expense({'_id': ObjectId(expense_id)})
+    if result.deleted_count:
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "success", "message": "expense deleted"})
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"expense with id {expense_id} not found")
